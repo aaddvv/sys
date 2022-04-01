c7a41d3d84117367f3ca16c79f9b3f3965ddd98a
aaddvv <102828429+aaddvv@users.noreply.github.com>
2022-04-01 22:05:37 +0300

    Add files via upload

---
+++ kernel.c
@@ -0,0 +1,28 @@
+void kmain(void)
+{
+	const char *str = "system loaded";
+	char *vidptr = (char*)0xb8000; 	//видео пямять начинается здесь
+	unsigned int i = 0;
+	unsigned int j = 0;
+
+	/* этот цикл очищает экран*/
+	while(j < 80 * 25 * 2) {
+		/* пустой символ */
+		vidptr[j] = ' ';
+		/* байт атрибутов */
+		vidptr[j+1] = 0x07; 		
+		j = j + 2;
+	}
+
+	j = 0;
+
+	/* в этом цикле строка записывается в видео память */
+	while(str[j] != '\0') {
+		/* ascii отображение */
+		vidptr[i] = str[j];
+		vidptr[i+1] = 0x07;
+		++j;
+		i = i + 2;
+	}
+	return;
+}
